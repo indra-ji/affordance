0965dac8a110fe42e891c5101c19e3e48a30a176
Indraji Wanigaratne <indrajiwanigaratne@192.168.1.2>
2025-08-12 02:04:49 +0530

    Load setup configs and create new evaluation

--- json_utils.py
+++ json_utils.py
@@ -1,16 +1,24 @@
+import json
+
 from data_models import BaseEntity
 
 
-def serialize_data_model(data_model: type[BaseEntity], output_path: str) -> None:
+def serialize_data_model(output_path: str, data_model: type[BaseEntity]) -> None:
     json_output = data_model.model_dump_json(indent=2)
 
-    with open(output_path, "w") as f:
+    with open(output_path, "w", encoding="utf-8") as f:
         f.write(json_output)
 
 
 def deserialize_data_model(
     input_path: str, model_class: type[BaseEntity]
 ) -> type[BaseEntity]:
-    with open(input_path, "r") as f:
+    with open(input_path, "r", encoding="utf-8") as f:
         json_data = f.read()
     return model_class.model_validate_json(json_data)
+
+
+def deserialize_dict(input_path: str) -> dict:
+    with open(input_path, "r", encoding="utf-8") as f:
+        json_data = f.read()
+    return json.loads(json_data)
