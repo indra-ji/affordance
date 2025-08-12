49a858893e597d5ea9f361ed9765d3e2bb4abf81
Indraji Wanigaratne <indrajiwanigaratne@192.168.1.2>
2025-08-13 01:26:14 +0530

    Added data for mini-numpy demo

--- json_utils.py
+++ json_utils.py
@@ -12,11 +12,11 @@ def serialize_data_model(output_path: str, data_model: type[BaseEntity]) -> None
 
 
 def deserialize_data_model(
-    input_path: str, model_class: type[BaseEntity]
+    input_path: str, data_model: type[BaseEntity]
 ) -> type[BaseEntity]:
     with open(input_path, "r", encoding="utf-8") as f:
         json_data = f.read()
-    return model_class.model_validate_json(json_data)
+    return data_model.model_validate_json(json_data)
 
 
 def deserialize_dict(input_path: str) -> dict:
